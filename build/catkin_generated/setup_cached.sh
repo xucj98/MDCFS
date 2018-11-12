8f56717968baf8f89b386efd0d546fae7b58d3c3
xucuijie <xucuijie@momenta.ai>
2018-11-12 19:23:35 +0800

    create roboware workspace

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/jay/Projects/MDCFS/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/jay/Projects/MDCFS/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/opt/ros/kinetic/bin:/home/jay/bin:/home/jay/.local/bin:/home/jay/bin:/home/jay/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+export PWD="/home/jay/Projects/MDCFS/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/jay/Projects/MDCFS/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/jay/Projects/MDCFS/src:$ROS_PACKAGE_PATH"

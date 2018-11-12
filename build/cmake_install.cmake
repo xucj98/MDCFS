83e7b79b325f8f1d05a78c3523518293e786ba70
xucuijie <xucuijie@momenta.ai>
2018-11-12 19:59:32 +0800

    create rospackage test_ros.

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -122,6 +122,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/jay/Projects/MDCFS/build/gtest/cmake_install.cmake")
+  include("/home/jay/Projects/MDCFS/build/test_ros/cmake_install.cmake")
 
 endif()
 

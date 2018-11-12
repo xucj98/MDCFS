1c580879ff474346e856ba83e8fc7bfdf6a9a99a
xucuijie <xucuijie@momenta.ai>
2018-11-12 20:10:09 +0800

    update .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jay/Projects/MDCFS/src
-# Build directory: /home/jay/Projects/MDCFS/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("test_ros")
